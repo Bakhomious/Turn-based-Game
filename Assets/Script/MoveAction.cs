33ca546add6040339419253595b5b903dfbb56a5
Bakhomious <bakhomioussvp@gmail.com>
2023-04-24 21:37:27 +0200

    Grid Visuals Implementation

--- Assets/Script/MoveAction.cs
+++ Assets/Script/MoveAction.cs
@@ -6,16 +6,20 @@ public class MoveAction : MonoBehaviour
 {
     [SerializeField] private Animator unitAnimator;
     private Vector3 targetPosition;
+    [SerializeField] private int maxMoveDistance = 4;
+
+    private Unit unit;
 
     void Awake()
     {
+        unit = GetComponent<Unit>();
         targetPosition = transform.position;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -33,14 +37,57 @@ public class MoveAction : MonoBehaviour
             transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
 
             unitAnimator.SetBool("isWalking", true);
-        } else {
+        }
+        else
+        {
             unitAnimator.SetBool("isWalking", false);
         }
     }
 
-    
-    public void Move(Vector3 targetPosition)
+    public void Move(GridPosition gridPosition)
     {
-        this.targetPosition = targetPosition;
+        this.targetPosition = LevelGrid.Instance.GetWorldPosition(gridPosition);
+    }
+
+    public bool IsValidActionGridPosition(GridPosition gridPosition)
+    {
+        List<GridPosition> validGridPositionList = GetValidActionGridPositionList();
+        return validGridPositionList.Contains(gridPosition);
+    }
+
+    public List<GridPosition> GetValidActionGridPositionList()
+    {
+        List<GridPosition> validGridPositions = new List<GridPosition>();
+        GridPosition unitGridPosition = unit.GetGridPosition();
+
+        for (int x = -maxMoveDistance; x <= maxMoveDistance; x++)
+        {
+            for (int z = -maxMoveDistance; z <= maxMoveDistance; z++)
+            {
+                GridPosition offsetGridPosition = new GridPosition(x, z);
+                GridPosition testGridPosition = unitGridPosition + offsetGridPosition;
+
+                if (!LevelGrid.Instance.IsValidGridPosition(testGridPosition))
+                {
+                    continue;
+                }
+
+                if (unitGridPosition == testGridPosition)
+                {
+                    // Unit is ON that position
+                    continue;
+                }
+
+                if (LevelGrid.Instance.HasAnyUnitOnGridPosition(testGridPosition))
+                {
+                    // Occupied Grid Position with another unit
+                    continue;
+                }
+                Debug.Log(testGridPosition);
+                validGridPositions.Add(testGridPosition);
+            }
+        }
+
+        return validGridPositions;
     }
 }
--- Assets/Script/Testing.cs
+++ Assets/Script/Testing.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
+    [SerializeField] private Unit unit;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,10 @@ public class Testing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            GridSystemVisual.Instance.HideAllGridPosition();
+            GridSystemVisual.Instance.ShowGridPositionList(unit.GetMoveAction().GetValidActionGridPositionList());
+        }
     }
 }
